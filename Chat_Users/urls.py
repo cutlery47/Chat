604d20b9f1ef64caa36db2423f5b2691ef02d703
Arkhip McNutt <ortiz47@mail.ru>
2023-08-30 22:42:40 +0300

    views are now class-based, got rid of view and url redundancy

--- Chat_Users/urls.py
+++ Chat_Users/urls.py
@@ -3,13 +3,7 @@ from . import views
 
 urlpatterns = [
     #для пользователя
-    path("", views.retrieveAllUsers, name='retrieve_all'),
-    path("<int:user_id>", views.retrieveSingleUser, name='retrieve_single'),
-    path("add/", views.addSingleUser, name='add_single'),
-    path("remove/<int:user_id>", views.removeSingleUser, name='remove_single'),
-    path("update/<int:user_id>", views.updateSingleUser, name='update_single'),
-
-    #для друзей пользователя
-    path("friends/<int:user_id>", views.retrieveUserFriends, name='user_friends')
+    path("", views.getOrCreateUsers.as_view(), name='get_create_users'),
+    path("<int:user_id>", views.retriveOrEditOrDeleteUser.as_view(), name='retrieve_single'),
 ]
 
--- Chat_Users/views.py
+++ Chat_Users/views.py
@@ -1,66 +1,62 @@
 from .models import User
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer
 
-#получить всех пользователей
-@api_view(['GET'])
-def retrieveAllUsers(request):
-    all_users = User.objects.all()
-    serialized_all_users = UserSerializer(all_users, many=True)
-    return Response(serialized_all_users.data)
+#представление для получения данных о всех юзерах и добавления пользователя
+class getOrCreateUsers(APIView):
+    def get(self, request):
+        all_users = User.objects.all()
+        all_users_ser = UserSerializer(all_users, many=True)
+        return Response(all_users_ser.data)
 
-#получить данные об одном пользователе
-@api_view(['GET'])
-def retrieveSingleUser(request, user_id):
-    user = User.objects.get(pk=user_id)
-    serialized_user = UserSerializer(user, many=False)
-    return Response(serialized_user.data)
+    def post(self, request):
+        serilized_user = UserSerializer(data=request.data)
+        if serilized_user.is_valid():
+            data = serilized_user.validated_data
+            User.objects.create_user(username=data['username'], first_name=data['first_name'],
+                                     last_name=data['last_name'], password=data['password'],
+                                     is_staff=data['is_staff'])
+            return Response(data)
+        else:
+            return Response(serilized_user.errors)
 
-#добавить пользователя
-@api_view(['POST'])
-def addSingleUser(request):
-    serilized_user = UserSerializer(data=request.data)
-    if serilized_user.is_valid():
-        data = serilized_user.data
-        User.objects.create_user(username=data['username'], first_name=data['first_name'], last_name=data['last_name'],
-                                 password=data['password'], is_staff=data['is_staff'])
-        return Response("The user has been added successfully")
-    else:
-        return Response(serilized_user.errors)
+#представление для получения одного юзера, редактирования юзера, удаления юзера
+class retriveOrEditOrDeleteUser(APIView):
+    def usercheck(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except:
+            return None
 
-#обновить данные пользователя
-#мне очень стыдно но пока что я не знаю как сделать это по-другому...
-#один огромный костыль
-@api_view(['POST'])
-def updateSingleUser(request, user_id):
-    user = User.objects.get(pk=user_id)
 
-    #пофиксить
-    if 'password' not in request.data.keys():
-        request.data['password'] = user.password
+    def get(self, request, **kwargs):
+        user = self.usercheck(kwargs['user_id'])
 
-    serialized_user = UserSerializer(instance=user, data=request.data)
+        if user:
+            user_ser = UserSerializer(user)
+            return Response(user_ser.data)
+        else:
+            return Response("USER NOT FOUND")
 
-    if serialized_user.is_valid():
-        user = serialized_user.save()
-        user.set_password(user.password)
-        user.save()
-        return Response("The user has been updated successfully!")
-    else:
-        return Response(serialized_user.errors)
+    def delete(self, request, **kwargs):
+        user = self.usercheck(kwargs['user_id'])
 
-#удалить пользователя
-@api_view(['POST'])
-def removeSingleUser(request, user_id):
-    user = User.objects.get(pk=user_id)
-    user.delete()
-    return Response("The user has been removed successfully!")
+        if user:
+            user.delete()
+            return Response("USER HAS BEEN SUCCESSFULLY DELETED")
+        else:
+            return Response("USER NOT FOUND")
 
-#получить друзей пользователя
-@api_view(['GET'])
-def retrieveUserFriends(request, user_id):
-    user = User.objects.get(pk=user_id)
-    friends = user.friends.all()
-    serialized_friends = UserSerializer(friends, many=True)
-    return Response(serialized_friends.data)
+    def put(self, request, **kwargs):
+        user = self.usercheck(kwargs['user_id'])
+
+        if user:
+            user_ser = UserSerializer(instance=user, data=request.data)
+            if user_ser.is_valid():
+                user_ser.save()
+                return Response("USER HAS BEEN UPDATED SUCCESSFULLY")
+            else:
+                return Response(user_ser.errors)
+        else:
+            return Response("USER NOT FOUND")
